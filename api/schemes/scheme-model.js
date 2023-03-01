f065ee0df9b3948654a5cf9bdbc8ce9427702ba6
Alita1093 <furkanozturk1635@gmail.com>
2023-03-02 02:03:59 +0300

    s14-g3

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,7 @@
-function find() { // Egzersiz A
+const db = require("../../data/db-config");
+
+function find() {
+  // Egzersiz A
   /*
     1A- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin.
     LEFT joini Inner joine çevirirsek ne olur?
@@ -15,9 +18,15 @@ function find() { // Egzersiz A
     2A- Sorguyu kavradığınızda devam edin ve onu Knex'te oluşturun.
     Bu işlevden elde edilen veri kümesini döndürün.
   */
+  return db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("sc.*", "st.step_id as number_of_steps")
+    .groupBy("sc.scheme_id")
+    .orderBy([{ column: "sc.scheme_id", order: "asc" }]);
 }
 
-function findById(scheme_id) { // Egzersiz B
+async function findById(scheme_id) {
+  // Egzersiz B
   /*
     1B- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin:
 
@@ -32,7 +41,6 @@ function findById(scheme_id) { // Egzersiz B
 
     2B- Sorguyu kavradığınızda devam edin ve onu Knex'te oluşturun
     parametrik yapma: `1` hazır değeri yerine `scheme_id` kullanmalısınız.
-
     3B- Postman'da test edin ve ortaya çıkan verilerin bir şema gibi görünmediğini görün,
     ancak daha çok her biri şema bilgisi içeren bir step dizisi gibidir:
 
@@ -83,9 +91,33 @@ function findById(scheme_id) { // Egzersiz B
         "steps": []
       }
   */
+  const schemeVali = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("sc.scheme_name", "st.*")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
+
+  const response = {
+    scheme_id: parseInt(scheme_id),
+    scheme_name: schemeVali[0].scheme_name,
+    steps: [],
+  };
+  if (schemeVali[0].step_number === null) {
+    return response;
+  } else {
+    schemeVali.forEach((item) => {
+      response.steps.push({
+        step_id: item.step_id,
+        step_number: item.step_number,
+        instructions: item.instructions,
+      });
+    });
+    return response;
+  }
 }
 
-function findSteps(scheme_id) { // Egzersiz C
+async function findSteps(scheme_id) {
+  // Egzersiz C
   /*
     1C- Knex'te aşağıdaki verileri döndüren bir sorgu oluşturun.
     Adımlar, adım_numarası'na göre sıralanmalıdır ve dizi
@@ -106,20 +138,32 @@ function findSteps(scheme_id) { // Egzersiz C
         }
       ]
   */
+  const stepVali = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("st.*", "sc.scheme_name")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
+  return !stepVali[0].step_id ? [] : stepVali;
 }
 
-function add(scheme) { // Egzersiz D
+async function add(scheme) {
+  // Egzersiz D
   /*
     1D- Bu işlev yeni bir şema oluşturur ve _yeni oluşturulan şemaya çözümlenir.
   */
+  const insterted = await db("schemes").insert(scheme);
+  return await findById(insterted);
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) {
+  // EXERCISE E
   /*
     1E- Bu işlev, verilen 'scheme_id' ile şemaya bir adım ekler.
     ve verilen "scheme_id"ye ait _tüm adımları_ çözer,
     yeni oluşturulan dahil.
   */
+  await db("steps").insert({ ...step, scheme_id });
+  return await findSteps(scheme_id);
 }
 
 module.exports = {
@@ -128,4 +172,4 @@ module.exports = {
   findSteps,
   add,
   addStep,
-}
+};
--- queries.sql
+++ queries.sql
@@ -1,9 +1,36 @@
 -- Multi-Table Sorgu Pratiği
 
 -- Tüm ürünler(product) için veritabanındaki ProductName ve CategoryName'i listeleyin. (77 kayıt göstermeli)
-
+select ProductName,CategoryName from Product p
+join Category c on p.CategoryId=c.Id
 -- 9 Ağustos 2012 öncesi verilmiş tüm siparişleri(order) için sipariş id'si (Id) ve gönderici şirket adını(CompanyName)'i listeleyin. (429 kayıt göstermeli)
-
+select o.Id as "SiparisId",c.CompanyName from [Order] o
+join Customer c on o.CustomerId=c.Id
+where OrderDate<"2012-08-09"
 -- Id'si 10251 olan siparişte verilen tüm ürünlerin(product) sayısını ve adını listeleyin. ProdcutName'e göre sıralayın. (3 kayıt göstermeli)
-
+select od.Quantity,p.ProductName from [OrderDetail] od
+join Product p on od.ProductId=p.Id
+where OrderId=10251
+order by p.ProductName
 -- Her sipariş için OrderId, Müşteri'nin adını(Company Name) ve çalışanın soyadını(employee's LastName). Her sütun başlığı doğru bir şekilde isimlendirilmeli. (16.789 kayıt göstermeli)
+select o.Id,c.CompanyName,e.LastName from[Order] o
+join Employee e on o.EmployeeId=e.Id
+join Customer c on o.CustomerId=c.Id
+--Her gönderici tarafından gönderilen gönderi sayısını bulun.
+select customerId,count(CustomerId) as"Siparis" from [Order] o
+group by CustomerId
+order by count(CustomerId) desc
+--Sipariş sayısına göre ölçülen en iyi performans gösteren ilk 5 çalışanı bulun.
+select o.EmployeeId,sum(o.EmployeeId) from [Order] o
+group by o.EmployeeId
+order by sum(o.EmployeeId) desc
+limit 5
+
+--Gelir olarak ölçülen en iyi performans gösteren ilk 5 çalışanı bulun.
+select o.EmployeeId,Sum(od.UnitPrice) as"Performans" from OrderDetail od
+join [Order] o on od.OrderId=o.Id
+group by o.EmployeeId
+order by Sum(od.UnitPrice) desc
+limit 5
+--En az gelir getiren kategoriyi bulun.
+--En çok siparişi olan müşteri ülkesini bulun.
